8b87987dda3db44fc506f6ef60f3feb39daa18a2
Mikko Luhtasaari <mikko.luhtasaari@tuni.fi>
2020-03-26 10:01:59 +0200

    Make example of jsdoc

--- assignment_template/js/index.js
+++ assignment_template/js/index.js
@@ -1,1 +1,10 @@
-console.log("Hello from assignment!");
+/**
+ * Writes given text to console.log
+ * @param {string} text Text to print out 
+ */
+const sayHello = (text) => console.log(text);
+
+(() => {
+  const helloIndex = "Hello from index.js!";
+  sayHello(helloIndex);
+})();
