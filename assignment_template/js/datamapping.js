ee5b98b9ea178e67613b6d763fdf3d7aa9749f72
nico <niko-petteri.alanko@tuni.fi>
2020-05-08 18:35:28 +0300

    Comment datamapping

--- assignment_template/js/datamapping.js
+++ assignment_template/js/datamapping.js
@@ -1,12 +1,24 @@
+/**
+ * colorNeighbours function updates d3 map to show the current corona situation
+ * of the selected country and the neighbouring countries of that country.
+ * Updates color fill of the coutnries to the d3 map.
+ * @param {Array<Object>} Receives countryycode from user input in table
+ */
 function colorNeighbors(countryCode) {
 
+	//Reset all colours to default fill
     map.updateChoropleth(null, {reset: true});
+	
+	//try-catch-block. Errors are caused by faulty or empty countrycodes
 	try {
+		//First paint the selected country
 		map.updateChoropleth({
 			[countryCode]: getColor(caseMap[countryCode].confirmed, caseMap[countryCode].deaths)
 		});
 		
+		//In a for-loop, paint the neighbouring countries of the selected country.
 		for (country in neighMap[countryCode]) {
+			//Try-catch-block, errors are caused by neighbouring countries that have no cases
 			try {
 				console.log(neighMap[countryCode][country]);
 				map.updateChoropleth({
@@ -23,9 +35,16 @@ function colorNeighbors(countryCode) {
 	}
 };
 
+/**
+ * initialFills-function colors the countries with their current corona stats.
+ * Updates color fill of the coutnries to the d3 map.
+ * @param {Array<Object>} mapped cases
+ */
 function initialFills(mappedcases) {
-	//console.log(mappedcases);
+	
+	//Loop thru the cases
 	for (country in mappedcases) {
+		//Try-catch-block, errors are caused by countries that have no cases
 		try {
 			console.log(country);
 			map.updateChoropleth({
@@ -40,9 +59,14 @@ function initialFills(mappedcases) {
 	
 };
 
+/**
+ * timeseriesHelper colors the countries with their corona stats.
+ * Method is called "per day"
+ * Updates color fill of the coutnries to the d3 map.
+ * @param {Array<Object>} @param {Array<Object>} receives object of corona stats for given day
+ */
 function timeseriesHelper(date, max) {
-	//console.log(date)
-	//console.log(date);
+
 	for (country in date) {
 		try {
 			map.updateChoropleth({
@@ -56,21 +80,38 @@ function timeseriesHelper(date, max) {
 	}
 }
 
+/**
+ * timeseriesFills colors the countries with their corona stats.
+ * Method is called once and receives object of dates, which contain corona stats to that date
+ * Updates color fill of the coutnries to the d3 map.
+ * @param {Array<Object>} receives object of corona stats
+ */
 function timeseriesFills(ts) {
+	//Helper variables
+	//Keys to the timeseries object
 	var keys = Object.keys(ts);
+	//counter
 	var i = 0;
+	//how many days
 	var max = keys.length;
 	var maxdate = ts[keys[max-1]];
 	//console.log(maxdate);
+	
+	//Reset map
 	map.updateChoropleth(null, {reset: true});
+	
+	//intervalling.
 	var inte = setInterval(function(){
 		console.log(keys[i]);
+		//Called once per second with iterated key
 		timeseriesHelper(ts[keys[i]], maxdate);
+		//If all dates have been looped thru, stop
 		if (typeof keys[i] === 'undefined') {
 			stop();
 		}
 		i++;
 	}, 200);
+	//Inner function to stop the intervalling
 	function stop() {
 		clearInterval(inte);
 	};
