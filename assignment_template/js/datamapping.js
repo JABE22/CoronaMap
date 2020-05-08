add77324c69fef11e9346364d8857baf20b06cee
JABE22 <37688643+JABE22@users.noreply.github.com>
2020-05-08 19:43:29 +0300

    Roadmap update
    Roadmap with prerequisities

--- assignment_template/js/datamapping.js
+++ assignment_template/js/datamapping.js
@@ -1,36 +1,37 @@
+
 function colorNeighbors(countryCode) {
 
     map.updateChoropleth(null, {reset: true});
-	try {
-		map.updateChoropleth({
-			[countryCode]: getColor(caseMap[countryCode].confirmed, caseMap[countryCode].deaths)
-		});
-		
-		for (country in neighMap[countryCode]) {
-			try {
-				console.log(neighMap[countryCode][country]);
-				map.updateChoropleth({
-					[neighMap[countryCode][country]]: getColor(caseMap[neighMap[countryCode][country]].confirmed, caseMap[neighMap[countryCode][country]].deaths)
-				});
-			}
-			catch (error) {
-				console.log("was nothing in that country");
-			}
-		}
-	}
-	
-	//console.log(countryCode);
-	//console.log(caseMap[countryCode].confirmed);
-	/*console.log(typeof caseMap[countryCode].confirmed);
-	let color = getColor(caseMap[countryCode].confirmed, caseMap[countryCode].deaths);
-	console.log(color);
-	try {
-		map.updateChoropleth({
-			[countryCode]: color
-		});
-		console.log(color);
-	}*/
-	catch (err) {
-		console.log(err);
-	}
-};
+    try {
+        map.updateChoropleth({
+            [countryCode]: getColor(caseMap[countryCode].confirmed, caseMap[countryCode].deaths)
+        });
+
+        for (country in neighMap[countryCode]) {
+            try {
+                console.log(neighMap[countryCode][country]);
+                map.updateChoropleth({
+                    [neighMap[countryCode][country]]: getColor(caseMap[neighMap[countryCode][country]].confirmed, caseMap[neighMap[countryCode][country]].deaths)
+                });
+            } catch (error) {
+                console.log("was nothing in that country");
+            }
+        }
+    }
+
+    //console.log(countryCode);
+    //console.log(caseMap[countryCode].confirmed);
+    /*console.log(typeof caseMap[countryCode].confirmed);
+     let color = getColor(caseMap[countryCode].confirmed, caseMap[countryCode].deaths);
+     console.log(color);
+     try {
+     map.updateChoropleth({
+     [countryCode]: color
+     });
+     console.log(color);
+     }*/
+    catch (err) {
+        console.log(err);
+    }
+}
+;
